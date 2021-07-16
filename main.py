9d4315b89ef9cebd096115bbb50996d60a325b95
Bruno Antunes <brunoantunes4242@gmail.com>
2021-07-16 20:09:24 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from pynput import keyboard
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Key, Listener
+import time
+
+mouse = Controller()
+
+inicia = False
+
+if key == Key.q:
+
