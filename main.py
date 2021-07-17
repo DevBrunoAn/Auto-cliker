c9fde01870ff76f6d178aa1e33aefc3f964fd7d2
Bruno Antunes <brunoantunes4242@gmail.com>
2021-07-17 02:50:31 -0300

    Update

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+from os import truncate
+from typing import Container
 from pynput import keyboard
 from pynput.mouse import Button, Controller
-from pynput.keyboard import Key, Listener
+import keyboard
 import time
 
 mouse = Controller()
 
 inicia = False
+contador = 0
 
-if key == Key.q:
+while contador == 0:
+
+    if inicia == False:
+        if keyboard.is_pressed("q"):
+            inicia = True
+    
+    if inicia == True:
+        while inicia == True:
+            mouse.click(Button.left, 1)
+            time.sleep(0.1)
+            if keyboard.is_pressed("p"):
+                inicia = False
+    
+    if keyboard.is_pressed("u"):
+            contador += 1
+            exit()
+
+    
+            
+
+
 
