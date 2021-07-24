a516dfeae6ae9c71f7fd4f4e7179824c38f39400
Bruno Antunes <brunoantunes4242@gmail.com>
2021-07-23 22:54:22 -0300

    remove inputs

--- main.py
+++ main.py
@@ -3,33 +3,28 @@ from typing import Container
 from pynput import keyboard
 from pynput.mouse import Button, Controller
 import keyboard
-import time
-import csv
 
-mouse = Controller()
+while True:
+    mouse = Controller()
 
-inicia = False
-contador = 0
+    inicia = False
+    contador = 0
 
-inicia_key = input("Digite a tecla para iniciar o cliker: ")
-desligar_key = input("Digite a tecla para desligar o cliker: ")
-sair_key = input("Digite a tecla para sair do auto cliker: ")
+    while contador == 0:
 
-while contador == 0:
+        if inicia == False:
+            if keyboard.is_pressed('o'):
+                inicia = True
 
-    if inicia == False:
-        if keyboard.is_pressed(inicia_key):
-            inicia = True
+        if inicia == True:
+            while inicia == True:
+                mouse.click(Button.left, 2)
+                # time.sleep(0.01)
+                if keyboard.is_pressed('p'):
+                    inicia = False
+                    print('Desligou')
 
-    if inicia == True:
-        while inicia == True:
-            mouse.click(Button.left, 2)
-            # time.sleep(0.01)
-            if keyboard.is_pressed(desligar_key):
-                inicia = False
-                print('Desligou')
-
-    if keyboard.is_pressed(sair_key):
-        contador += 1
-        print('Fechou')
-        exit()
+        if keyboard.is_pressed('i'):
+            contador += 1
+            print('Fechou')
+            exit()
